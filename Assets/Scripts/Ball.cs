609c9ddd95450f45d502f3ed1ae3ca958882517e
Shaowei <Shaowei.Zhang@Outlook.com>
2019-02-19 21:28:05 +1100

    Add 3d ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private float launchSpeed;
+    [SerializeField] private AudioSource audioSource;
+    private Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
+        Launch();
+    }
+
+    public void Launch()
+    {
+        _rigidbody.velocity = new Vector3(0, 0, launchSpeed);
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
